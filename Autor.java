62cf8e6db2ed2c4df94fcb5c9f7a38607da07cc0
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 15:52:49 -0300

    Update Autor.java

--- Autor.java
+++ Autor.java
@@ -1,1 +1,18 @@
+//João Victor de Jesus Augusto
+public class Autor extends Pessoa {
+    private boolean isUsuario;
+
+    public Autor(String nome, int idade, boolean isUsuario) {
+        super(nome, idade);
+        this.isUsuario = isUsuario;
+    }
+
+    public boolean isUsuario() {
+        return isUsuario;
+    }
+
+    public void setUsuario(boolean isUsuario) {
+        this.isUsuario = isUsuario;
+    }
+}
 
