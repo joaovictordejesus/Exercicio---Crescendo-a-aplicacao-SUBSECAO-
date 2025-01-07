d01fa13acd618bd1f76ed97de34b5bc1008437d9
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 15:53:23 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,1 +1,46 @@
+//João Victor de Jesus Augusto
+public class Livro {
+    private String titulo;
+    private Autor autor;
+    private String genero;
+    private boolean disponivel;
 
+    public Livro(String titulo, Autor autor, String genero, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.disponivel = disponivel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
