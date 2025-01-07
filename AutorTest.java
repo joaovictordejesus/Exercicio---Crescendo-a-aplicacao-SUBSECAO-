c7a71ce484bac1f0d299cfe7c758fe3fb702006e
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 15:57:45 -0300

    Update AutorTest.java

--- AutorTest.java
+++ AutorTest.java
@@ -1,1 +1,17 @@
+// João Victor de Jesus Augusto
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class AutorTest {
+    @Test
+    public void testIsUsuario() {
+        Autor autor = new Autor("Jess", 30, true);
+        assertTrue(autor.isUsuario());
+    }
+    @Test
+    public void testSetUsuario() {
+        Autor autor = new Autor("Jess", 30, false);
+        autor.setUsuario(true);
+        assertTrue(autor.isUsuario());
+    }
+}
